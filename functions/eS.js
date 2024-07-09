8e92ca8c05acbd7638865956f67910c775971442
Carlos17Ulibarri <carheuc@gmail.com>
2024-07-09 14:31:28 -0600

    Expressions vs Statements

---
+++ functions/eS.js
@@ -0,0 +1,12 @@
+//Expressions vs Statements 
+/*
+    Expression: something that generates value
+    Examples:
+        'Hola'
+        5 + 10
+    
+    Statement: code fragment that generates instructions on the program
+    Example: 
+        const intNumber = 5;
+
+*/
