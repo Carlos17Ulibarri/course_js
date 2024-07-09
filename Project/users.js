6f02f160ce3d43ead2946d9407c18331c414e5a3
Carlos17Ulibarri <carheuc@gmail.com>
2024-07-09 14:32:02 -0600

    Project progress

--- Project/users.js
+++ Project/users.js
@@ -1,6 +1,6 @@
 // Information users
-
-const users = [
+/*
+const users = [ //structure
     u1= {
         username: 'Consultor1',
         fullName: 'Jonh Martinez',
@@ -15,3 +15,32 @@ const users = [
         perBio: `I'm ${u1.fullName}, I like too the innovation`
     },
 ];
+*/
+
+function createUser(name,username,password,profile){
+    this.name = name;
+    this.username = username;
+    this.password = password;
+    this.profile = profile;
+
+    this.getUsers = function getUsers(){
+        console.log(`Name: ${this.name}\nUsername: ${this.username}\nProfile: ${this.profile}`);
+    };
+
+    this.changeProfile = function changeProfile(){
+        this.profile = 'Administrator';
+    };
+};
+
+const users = {
+    u1: new createUser('carlos','carlos17',"nfonsfoj","Operations"),
+    u2: new createUser('Karla','kl98',"asopkd","Operations"),
+    u3: new createUser('Oscar','ocr36',"5555","Operations"),
+}
+
+
+
+
+users.u2.getUsers();
+users.u2.changeProfile();
+users.u2.getUsers();
